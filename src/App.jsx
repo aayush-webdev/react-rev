752d26a0a9dfb78475710dcd3660a34d9162b7af
Aayush Sharma <your-email@example.com>
2026-04-13 21:41:28 +0530

    Add lucide icons and update Card component layout with new design

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,9 @@ import Card from './components/Card'
 const App = () => {
   return (
     <div className="parent">
-      <Card user = "aayush sharma"
-        userImg = "https://images.unsplash.com/photo-1708024587407-73445142b5a8?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        userBio = "hii i am aayush sharma and i am a web dev"
-        
-      />
-      <Card user="sumit sharma"
-      userImg="https://images.unsplash.com/photo-1756086807815-d8c4676197dc?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%"
-      userBio="hi mera naam sumit hai aur aur "
-      />
-      <Card/>
-      <Card/>
-
-        </div>
+      <Card />
+     
+    </div>
   )
 }
 
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,32 @@
+import { Bookmark } from 'lucide-react'
 import React from 'react'
 
-const Card = (props) => {
-    console.log(props);
-    
+const Card = () => {
     return (
         <div className='card'>
-                <img src={props.userImg} alt={props.user} />
-                <h1>{props.user}</h1>
-                <p>{props.userBio}</p>
-                <button>View Profile</button>
+            <div>
+                <div className="top">
+                    <img src="https://images.unsplash.com/photo-1774804819708-61c4481d193d?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+                    <button>Save<Bookmark size={10} /></button>
+                </div>
+                <div className="center">
+                    <h3>Amazon <span>5 days ago</span></h3>
+                    <h2>Senior UI/UX Designer</h2>
+                    <div className='tag'>
+                        <h4>Part time </h4>
+                        <h4>Senior level</h4>
+
+                    </div>
+                </div></div>
+            <div className="bottom">
+                <div>
+                    <h3>$124/hr</h3>
+                    <p>Mumbai, India</p>
+                </div>
+                <button>Apply Now</button>
             </div>
+
+        </div>
     )
 }
 
